4b6bd549fe98f27d4ffc834bd1cda1e9fb7b19e6
Simon Delcourt <delcourt.simon@gmail.com>
2014-06-26 09:45:50 +0200

    First commit.

---
+++ setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Installer for the imio.helpers package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+
+setup(
+    name='imio.helpers',
+    version='0.1',
+    description="Various helper methods for development.",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='plone helpers utils dev imio',
+    author='Simon Delcourt',
+    author_email='simon.delcourt@imio.be',
+    url='http://pypi.python.org/pypi/imio.helpers',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['imio'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'five.grok',
+        'plone.api',
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'ecreall.helpers.testing',
+            'plone.app.testing',
+            'plone.app.robotframework',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
