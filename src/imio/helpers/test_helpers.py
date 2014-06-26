20c58165dd46063e76e67c5594981e760341ad49
Simon Delcourt <delcourt.simon@gmail.com>
2014-06-26 10:58:00 +0200

    Add base class for Browser tests.

---
+++ src/imio/helpers/test_helpers.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.testing import login
+
+from plone.testing.z2 import Browser
+
+import unittest
+
+
+class BaseTest(unittest.TestCase):
+    """
+    Helper class for tests.
+    """
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+
+class BrowserTest(BaseTest):
+    """
+    Helper class for Browser tests.
+    """
+
+    def setUp(self):
+        super(BrowserTest, self).setUp()
+        self.browser = Browser(self.portal)
+        self.browser.handleErrors = False
+
+    def browser_login(self, user, password):
+        login(self.portal, user)
+        self.browser.open(self.portal.absolute_url() + '/logout')
+        self.browser.open(self.portal.absolute_url() + "/login_form")
+        self.browser.getControl(name='__ac_name').value = user
+        self.browser.getControl(name='__ac_password').value = password
+        self.browser.getControl(name='submit').click()
