a5a381fc73f51f92b7c43d335cb1c291eb086d5c
Gauthier Bastien <g.bastien@imio.be>
2014-08-27 14:47:44 +0200

    Back to development: 0.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='imio.helpers',
-    version='0.2',
+    version='0.3.dev0',
     description="Various helper methods for development.",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
