1da417ccd8aa793a8033c3c79421d542f26b843a
Gauthier Bastien <g.bastien@imio.be>
2014-09-19 16:18:24 +0200

    Do not consider things empty to easily : if tree produce text, has children or has attributes, we consider it not empty

--- src/imio/helpers/tests/test_xhtml.py
+++ src/imio/helpers/tests/test_xhtml.py
@@ -27,14 +27,17 @@ class TestXHTMLModule(IntegrationTestCase):
                         '<p>Some text to keep</p>\n')
         self.assertTrue(removeBlanks('<p>Text line 1</p><p>Text line 2</p>') ==
                         '<p>Text line 1</p>\n<p>Text line 2</p>\n')
+        self.assertTrue(removeBlanks('<p><img src="my_image"/></p>') ==
+                        '<p>\n  <img src="my_image"/>\n</p>\n')
         # complex tree filled
         self.assertTrue(removeBlanks('<ul><li>First line</li><li>second line</li></ul>') ==
                         '<ul>\n  <li>First line</li>\n  <li>second line</li>\n</ul>\n')
         # complex tree semi-filled
         self.assertTrue(removeBlanks('<ul><li>First line</li><li></li></ul>') ==
                         '<ul>\n  <li>First line</li>\n  <li/>\n</ul>\n')
-        # empty complex tree
-        self.assertTrue(removeBlanks('<ul><li></li><li></li></ul>') == '')
+        # empty complex tree, is not wiped out because master tag containing children
+        self.assertTrue(removeBlanks('<ul><li></li><li></li></ul>') ==
+                        '<ul>\n  <li/>\n  <li/>\n</ul>\n')
 
     def test_xhtmlContentIsEmpty(self):
         """
@@ -43,13 +46,17 @@ class TestXHTMLModule(IntegrationTestCase):
         self.assertTrue(xhtmlContentIsEmpty(''))
         self.assertTrue(xhtmlContentIsEmpty(None))
         self.assertTrue(xhtmlContentIsEmpty('<p></p>'))
-        self.assertTrue(xhtmlContentIsEmpty('<br><br /><br/>'))
+        self.assertTrue(xhtmlContentIsEmpty('<br/><br/><br/>'))
         self.assertTrue(xhtmlContentIsEmpty('<p></p><p>&nbsp;</p><p> </p><b></b><i> </i>'))
-        # complex empty tree
-        self.assertTrue(xhtmlContentIsEmpty('<table><tr><td> </td><td>&nbsp;</td></tr></table>'))
+        self.assertTrue(xhtmlContentIsEmpty('<img />'))
+        # empty <p> with an attribute, considered not empty
+        self.assertTrue(not xhtmlContentIsEmpty('<p class="special_empty_line"></p>'))
+        # tree with children, considered not empty
+        self.assertTrue(not xhtmlContentIsEmpty('<table><tr><td> </td><td>&nbsp;</td></tr></table>'))
         self.assertTrue(not xhtmlContentIsEmpty("Some text without any tag"))
         self.assertTrue(not xhtmlContentIsEmpty("<p>Some broken HTML<broken_tag>text<broken_tag> </i></div>"))
         self.assertTrue(not xhtmlContentIsEmpty('<p>Some text to keep</p>'))
         self.assertTrue(not xhtmlContentIsEmpty('<p>&nbsp;</p><p>Some text to keep</p><i>&nbsp;</i>'))
         self.assertTrue(not xhtmlContentIsEmpty('<p>&nbsp;</p><i>Some text to keep</i>'))
         self.assertTrue(not xhtmlContentIsEmpty('<table><tr><td>Some text to keep</td><td>&nbsp;</td></tr></table>'))
+        self.assertTrue(not xhtmlContentIsEmpty('<p><img src="my_image_path.png"/></p>'))
--- src/imio/helpers/xhtml.py
+++ src/imio/helpers/xhtml.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
 import lxml.html
+import types
+
+
+def xhtmlContentIsEmpty(xhtmlContent):
+    '''This method checks if given p_xhtmlContent will produce someting on rendering.
+       p_xhtmlContent can either be a string or already a lxml.html element.'''
+    # first check if xhtmlContent is not simply None or so
+    isStr = isinstance(xhtmlContent, types.StringType) or isinstance(xhtmlContent, types.NoneType)
+    if isStr and (not xhtmlContent or not xhtmlContent.strip()):
+        return True
+
+    if isStr:
+        xhtmlContent = "<special_tag>%s</special_tag>" % xhtmlContent
+        tree = lxml.html.fromstring(unicode(xhtmlContent, 'utf-8'))
+    else:
+        tree = xhtmlContent
+    if tree.tag == 'special_tag':
+        if tree.getchildren():
+            for el in tree.getchildren():
+                if bool(el.text_content().strip()) or bool(el.attrib) or bool(el.getchildren()):
+                    return False
+            return True
+
+    # if xhtmlContent renders text or has attributes or has children, we consider it not empty
+    return not bool(tree.text_content().strip()) and not bool(tree.attrib) and not bool(tree.getchildren())
 
 
 def removeBlanks(xhtmlContent):
@@ -12,17 +37,7 @@ def removeBlanks(xhtmlContent):
     tree = lxml.html.fromstring(unicode(xhtmlContent, 'utf-8'))
     for el in tree.getchildren():
         # el can be a subtree, like <ul><li>...</li></ul> we must consider entire rendering of it
-        if not el.text_content().strip():
+        if xhtmlContentIsEmpty(el):
             el.getparent().remove(el)
     # only return children of the <special_tag>
     return ''.join([lxml.html.tostring(x, encoding='utf-8', pretty_print=True, method='xml') for x in tree.iterchildren()])
-
-
-def xhtmlContentIsEmpty(xhtmlContent):
-    '''This method checks if given p_xhtmlContent will produce someting on rendering.'''
-    # first check if xhtmlContent is not simply None or so
-    if not xhtmlContent or not xhtmlContent.strip():
-        return True
-
-    tree = lxml.html.fromstring(unicode(xhtmlContent, 'utf-8'))
-    return not bool(tree.text_content().strip())
