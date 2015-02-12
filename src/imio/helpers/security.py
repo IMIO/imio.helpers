fdae66ca104419d6cfb028370837f63d04cc3437
Stephan Geulette <s.geulette@imio.be>
2015-02-12 12:19:31 +0100

    Propose by default a more readable password

--- src/imio/helpers/security.py
+++ src/imio/helpers/security.py
@@ -56,7 +56,7 @@ def is_develop_environment():
         return False
 
 
-def generate_password(length=10, digits=1, upper=1, lower=1, special=1):
+def generate_password(length=10, digits=3, upper=2, lower=1, special=1, readable=True):
     """
         Create a random password with the specified length and minimum numbers of
         digit, upper and lower case letters, special characters.
@@ -67,15 +67,18 @@ def generate_password(length=10, digits=1, upper=1, lower=1, special=1):
     uppercase = string.uppercase.translate(None, "O")
     # string.punctuation contains !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~
     specials = '!#$%&*+-<=>?@'
-    letters = "{0:s}{1:s}".format(lowercase, uppercase)
+    #letters = "{0:s}{1:s}".format(lowercase, uppercase)
+    letters = "{0:s}".format(lowercase)
 
     password = list(
         chain(
             (choice(uppercase) for _ in range(upper)),
             (choice(lowercase) for _ in range(lower)),
-            (choice(string.digits) for _ in range(digits)),
+            (choice(letters) for _ in range((length - digits - upper - lower - special))),
             (choice(specials) for _ in range(special)),
-            (choice(letters) for _ in range((length - digits - upper - lower - special)))
+            (choice(string.digits) for _ in range(digits)),
         )
     )
-    return "".join(sample(password, len(password)))
+    if not readable:
+        password = sample(password, len(password))
+    return "".join(password)
