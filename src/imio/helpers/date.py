03c48d51b346322012b3ac7c07b973b547566f01
Vivian Antoine <vivian.antoine@imio.be>
2016-05-25 13:02:14 +0200

    Makes the code reviewer happy and correct a problem with blank spaces missing at the end of numbers between 0 and 9.

--- src/imio/helpers/date.py
+++ src/imio/helpers/date.py
@@ -4,7 +4,7 @@ from imio.helpers.testing import IntegrationTestCase
 from zope.i18n import translate
 
 
-monthsIds = {1:  'month_jan', 2:  'month_feb', 3:  'month_mar', 4:  'month_apr',
+MONTHIDS = {1:  'month_jan', 2:  'month_feb', 3:  'month_mar', 4:  'month_apr',
              5:  'month_may', 6:  'month_jun', 7:  'month_jul', 8:  'month_aug',
              9:  'month_sep', 10: 'month_oct', 11: 'month_nov', 12: 'month_dec'}
 
@@ -26,7 +26,11 @@ def formatDate(date, month_name=True, context=None, long_format=False):
     #check if we need to return the month name
     if month_name:
         month = date.month()
-        translated_month = translate(msgid=monthsIds[month], domain="plonelocales", context=context, default=monthsIds[month], target_language='fr')
+        translated_month = translate(msgid=MONTHIDS[month],
+                                     domain="plonelocales",
+                                     context=context,
+                                     default=MONTHIDS[month],
+                                     target_language='fr')
         #use mOntH here to avoid strftime translating the month...
         res = date.strftime('%e mOntH %Y' + hour)
         #replace '01 ' by '1er '
@@ -42,7 +46,10 @@ def wordizeDate(date, context=None, long_format=False):
       Convert a date made of digits to a date made of words.
       If long_format is True, we convert the hour to words too
     """
-    converted_month = formatDate(date, month_name=True, context=context, long_format=False).split(' ')[1]
+    converted_month = formatDate(date,
+                                 month_name=True,
+                                 context=context,
+                                 long_format=False).split(' ')[1]
 
     splitted_date = date.strftime("%Y/%m/%d").split('/')
     date_time = ""
@@ -57,7 +64,9 @@ def wordizeDate(date, context=None, long_format=False):
     else:
         converted_day = int2word(day_to_convert)
     converted_year = int2word(splitted_date[0])
-    converted_date = "%s %s %s" % (converted_day, converted_month, converted_year)
+    converted_date = u"{} {} {}".format(converted_day,
+                                        converted_month,
+                                        converted_year)
 
     converted_time = ""
     if long_format and date_time:
@@ -66,8 +75,8 @@ def wordizeDate(date, context=None, long_format=False):
         hour = splitted_time[0]
         minutes = splitted_time[1]
         converted_hour = int2word(int(hour))
-        if converted_hour.endswith("un "):
-            converted_hour = converted_hour[:-1] + "e"
+        if converted_hour.endswith("un"):
+            converted_hour += "e"
         converted_minutes = int2word(int(minutes))
         if hour in ['01', '1', '0', '00', ]:
             hour_word = "heure"
@@ -76,10 +85,15 @@ def wordizeDate(date, context=None, long_format=False):
         #do not display 'zero' in the date like : eleven hour zero...
         #see http://dev.communesplone.org/trac/ticket/1553
         if converted_minutes and not minutes in ['0', '00',]:
-            converted_time = " %s %s %s %s" % (u"à", converted_hour, hour_word, converted_minutes)
+            converted_time = u" {} {} {} {}".format(u"à",
+                                                    converted_hour,
+                                                    hour_word,
+                                                    converted_minutes)
         else:
             #remove the minutes, especially useless spaces if there are no minutes
-            converted_time = " %s %s %s" % (u"à", converted_hour, hour_word)
+            converted_time = u" {} {} {}".format(u"à",
+                                                 converted_hour,
+                                                 hour_word)
     return converted_date.encode('utf-8') + converted_time.encode('utf-8')
 
 def int2word(n):
@@ -89,20 +103,21 @@ def int2word(n):
       code that convert into english words...
     """
     ############# globals ################
-    ones = ["", "un ","deux ","trois ","quatre ", "cinq ",
+    ONES = ["", "un ","deux ","trois ","quatre ", "cinq ",
     "six ","sept ","huit ","neuf "]
-    tens = ["dix ","onze ","douze ","treize ", "quatorze ",
+    TENS = ["dix ","onze ","douze ","treize ", "quatorze ",
     "quinze ","seize ","dix-sept ","dix-huit ","dix-neuf "]
-    twenties = ["","","vingt ","trente ","quarante ",
+    TWENTIES = ["","","vingt ","trente ","quarante ",
     "cinquante ","soixante ","septante ","quatre-vingt ","nonante "]
-    thousands = ["","mille ","million ", "milliard ", "billiard ",
+    THOUSANDS = ["","mille ","million ", "milliard ", "billiard ",
     "trilliard ", "quadrilliard ", "quintillard ", "sextilliard", "septilliard ",
-    "octilliard ", "nonilliard ", u"dÃ©cillion ", u"dÃ©cilliard ", u"unodÃ©cillion ", u"unodÃ©cilliard ", u"duodÃ©cillion ", u"duodÃ©cilliard "]
+    "octilliard ", "nonilliard ", u"décillion ", u"décilliard ", u"unodécillion ",
+    u"unodécilliard ", u"duodécillion ", u"duodécilliard "]
 
     #quickly manage if n is < 10
     if n < 10:
-        ones = [u'zéro', "un ","deux ","trois ","quatre ", "cinq ", "six ","sept ","huit ","neuf "]
-        return ones[n]
+        ONES = [u"zéro ","un ","deux ","trois ","quatre ", "cinq ", "six ","sept ","huit ","neuf "]
+        return ONES[n].strip()
 
     # break the number into groups of 3 digits using slicing
     # each group representing hundred, thousand, million, billion, ...
@@ -110,7 +125,7 @@ def int2word(n):
     r1 = ""
     # create numeric string
     ns = str(n)
-    for k in range(3, 33, 3):
+    for k in range(3, 60, 3):
         r = ns[-k:]
         q = len(ns) - k
         # break if end of ns has been reached
@@ -126,39 +141,37 @@ def int2word(n):
 
     r1 = r
 
-    #print n3 # test
     # break each group of 3 digits into
-    # ones, tens/twenties, hundreds
+    # ONES, TENS/TWENTIES, hundreds
     # and form a string
     nw = ""
     for i, x in enumerate(n3):
         b1 = x % 10
         b2 = (x % 100)//10
         b3 = (x % 1000)//100
-        #print b1, b2, b3 # test
         if x == 0:
             continue # skip
         else:
-            t = thousands[i]
+            t = THOUSANDS[i]
         if b2 == 0:
             #cas du 'mille' qui n'est pas 'un mille'
             if b1 == 1 and i == 1:
                 nw = t + nw
             else:
-                nw = ones[b1] + t + nw
+                nw = ONES[b1] + t + nw
         elif b2 == 1:
-            nw = tens[b1] + t + nw
+            nw = TENS[b1] + t + nw
         elif b2 > 1:
             extra = ""
             if b1==1:
-                nw = twenties[b2] + "et un " + t + nw
+                nw = TWENTIES[b2] + "et un " + t + nw
             elif b1>1:
-                nw = twenties[b2][:-1] + "-" + ones[b1] + t + nw
+                nw = TWENTIES[b2][:-1] + "-" + ONES[b1] + t + nw
             else:
-                nw = twenties[b2] + extra + ones[b1] + t + nw
+                nw = TWENTIES[b2] + extra + ONES[b1] + t + nw
         if b3 > 0:
             if b3>1:
-                nw = ones[b3] + "cent " + nw
+                nw = ONES[b3] + "cent " + nw
             else:
                 nw = "cent " + nw
     return nw.strip()
