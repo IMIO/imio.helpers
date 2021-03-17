ecadd3069424bc0bbb57b50632cc5d50de9ca0d7
Gauthier Bastien <g.bastien@imio.be>
2021-03-17 08:56:58 +0100

    Forgot to add converters.py

---
+++ src/imio/helpers/converters.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from cgi import escape
+from ZPublisher.Converters import type_converters
+
+import json
+
+
+# create type converter for :json
+if 'field2json' not in type_converters:
+    def field2json(v):
+        try:
+            v = json.loads(v)
+        except ValueError:
+            raise ValueError("Invalid json " + escape(repr(v)))
+        return v
+    type_converters['json'] = field2json
