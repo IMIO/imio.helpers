8f638ee23ddc446adf3eb06d5f8f2a555f25beb3
Gauthier Bastien <gauthier.bastien@imio.be>
2022-06-01 14:04:05 +0200

    Invalidate cache when user added/deleted

--- src/imio/helpers/events.py
+++ src/imio/helpers/events.py
@@ -3,6 +3,16 @@
 from imio.helpers.cache import invalidate_cachekey_volatile_for
 
 
+def onPrincipalCreated(event):
+    """Invalidate the cachekey that manages users/groups associations."""
+    invalidate_cachekey_volatile_for('_users_groups_value', get_again=True)
+
+
+def onPrincipalDeleted(event):
+    """Invalidate the cachekey that manages users/groups associations."""
+    invalidate_cachekey_volatile_for('_users_groups_value', get_again=True)
+
+
 def onPrincipalAddedToGroup(event):
     """Invalidate the cachekey that manages users/groups associations."""
     invalidate_cachekey_volatile_for('_users_groups_value', get_again=True)
