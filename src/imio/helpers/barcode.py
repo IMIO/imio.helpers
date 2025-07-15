1c8800485ef5f0291b5dbd119a733da3f3b26459
Antoine Duchêne <antoine.duchene@imio.be>
2025-07-15 15:07:05 +0200

    Improve generate_barcode (#39)

--- src/imio/helpers/barcode.py
+++ src/imio/helpers/barcode.py
@@ -4,17 +4,16 @@ import six
 import subprocess
 
 
-def generate_barcode(data, executable="zint", barcode=92, scale=2, filetype="PNG"):
-    """Generate a barcode with zint in StringIO and return it
-
+def generate_barcode(data, executable='zint', barcode=92, scale=2, filetype='PNG', extra_args=None):
+    """Generate a barcode with zint in BytesIO and return it
     :param data: barcode content string
     :param executable: executable used to generate barcode
     :param barcode: barcode type number
     :param scale: scale of generated output
-    :param filetype: image type
-    :return: StringIO handler
+    :param filetype: image
+    :param extra_args: additional arguments for zint, for example ["--secure=2"]
+    :return: BytesIO handler
     """
-
     output = six.BytesIO()
 
     command = [
@@ -24,9 +23,16 @@ def generate_barcode(data, executable="zint", barcode=92, scale=2, filetype="PNG
         "--filetype={0}".format(filetype),
         "--barcode={0}".format(barcode),
         "--scale={0}".format(scale),
-        "--data={0}".format(data),
     ]
+    if extra_args:
+        command += extra_args
+    # Data must be the last argument
+    command.append("--data={0}".format(data))
+
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     output.write(process.stdout.read())
+    process.stdout.close()
+    process.wait()
+
     output.seek(0)
     return output
